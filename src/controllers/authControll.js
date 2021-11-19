536164e066cc0057484b3e5299704bb4345eef49
Diego <lima.j.diego@gmail.com>
2021-11-19 11:09:42 -0300

    criado o controler de autentificação

---
+++ src/controllers/authControll.js
@@ -0,0 +1,17 @@
+const express  = require('express')
+
+const User = require('../models/user')
+
+
+const router = express.Router() // buscando a classe Router do express
+
+router.post('/register', async (req, res) =>{
+    try {
+        const user =  await User.create(req.body)
+
+        return res.send({ user })
+        
+    }catch (error){
+        return res.status(400).send({ error: 'falha ao registrar'})
+    }
+})
