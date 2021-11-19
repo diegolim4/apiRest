1d11e396f3b01f9465b68e2e949bc9b07c5b370d
Diego <lima.j.diego@gmail.com>
2021-11-19 10:53:48 -0300

    model de usuário definido

---
+++ src/models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+// schema são os campos da tabela do usuários   
+const UserSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        require: true
+    },
+    email:{
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false  // para qndo trazer os usuários do db não trazer a senha 
+    },
+    createdAt:{ 
+        type: Date,
+        default: Date.now  // data em que o registro vai ser criado
+
+    },
+});
+
+// definir o model:
+
+const User = mongoose.model('User', UserSchema)
+
+module.exports = User
+
