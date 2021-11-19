0593de9c6a81474d678b6b556c3c36505031c12a
Diego <lima.j.diego@gmail.com>
2021-11-19 09:31:54 -0300

    Comentando o código

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-const express = require ('express')
+const express = require ('express')  
 const bodyParser = require('body-parser')
 
-const app = express()
+const app = express()   // core da aplicação
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())  //  Para que ele entenda quando eu enviar uma requisição em json 
+app.use(bodyParser.urlencoded({extended: false})) // para entender e decodar quando passar parâmetros via url
 
+// rotas
 app.get('/', (req, res)=> {
     res.send('Hello world')
 })
 
+// Definindo a rota
 const PORT = 1200
 app.listen(PORT, ()=>{
     console.log('Rodando em http://localhost:'+ PORT)
